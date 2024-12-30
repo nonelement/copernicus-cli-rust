d3781b1d93bea4bbad1132cf07d310160eb33f95
Anthony Kirkpatrick <anthony.j.kirkpatrick@gmail.com>
2024-12-30 11:39:52 -0500

    add spinners, redo formatting, add data from response

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ extern crate log;
 extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
+extern crate spinners;
 extern crate tokio;
 
 use std::env::var;
 use std::error::Error;
 use dotenv::dotenv;
 use serde::{Serialize, Deserialize};
+use spinners::{Spinner, Spinners};
 use clap::Parser;
 
 mod api;
@@ -65,14 +67,30 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut config: Config = confy::load(APP_NAME, None)?;
     let credentials = get_env_creds();
 
+    // Check provided user name to see if it has a reasonable value, e.g. not
+    // the template value, and not None. If it doesn't, we can't auth. We could
+    // reauth within the refresh window with cached auth, but we can probably
+    // insist on this.
+    match credentials.user {
+        Some(ref user) => if user == "FAKE_USER" {
+            panic!("Template value present in env credentials. Check values?");
+        },
+        None => panic!("No env value for user. Check credentials.")
+    }
+
     let client = reqwest::Client::new();
 
+    let mut s = Spinner::new(Spinners::Dots, "Checking auth...".into());
     let auth_details = check_auth(config.auth_details, &credentials).await?;
+    s.stop_with_newline();
+
+    // Save auth details
     config.auth_details = Some(auth_details.clone());
     confy::store(APP_NAME, None, config)?;
 
+    let mut s = Spinner::new(Spinners::Dots, "Querying for imagery at bbox...".into());
     let fc = list_imagery(&client, &auth_details, args.query_bbox_string).await?;
-
+    s.stop_with_newline();
     println!("features:\n{}", format_feature_collection(&fc));
 
     Ok(())
--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,19 @@
+use std::collections::HashMap;
 use geojson::{Feature, FeatureCollection};
 use geojson::feature::Id;
+use geojson::JsonObject;
+use geojson::JsonValue;
 use serde_json::map::Map;
+use serde_json::Value;
+
+const FEATURE_DETAILS_FORMAT: &str = r#"
+<ID> (<SHORT_NAME>.<SERIAL>/<DETAIL>)
+<CAPTURE_TIME> cloudy:<CLOUD_COVER>
+bbox: <BBOX>
+quicklook: <QUICKLOOK_HREF>
+archive: <PRODUCT_HREF>
+"#;
+
 
 pub fn format_feature_collection(fc: &FeatureCollection) -> String {
     let mut output: Vec<String> = Vec::new();
@@ -10,17 +23,52 @@ pub fn format_feature_collection(fc: &FeatureCollection) -> String {
     return output.join("\n");
 }
 
+fn get_value(path: Vec<&str>, m: &Option<JsonObject>) -> JsonValue {
+    let mut v: &JsonObject = if let Some(v) = m { v } else { return JsonValue::Null };
+    let mut t: JsonValue = Value::Null;
+    for name in path {
+        let _v: &JsonValue = &v[name];
+        match _v {
+            serde_json::Value::Object(obj) => v = &obj,
+            other => t = other.clone(),
+        }
+    }
+    return t;
+}
+
 pub fn format_feature(f: &Feature) -> String {
     // Top level feature attributes
     let id = match &f.id { Some(Id::String(v)) => v.clone(), Some(Id::Number(n)) => n.to_string(), None => String::new() };
-    let bbox = f.bbox.clone().unwrap_or(vec![]).iter().map(|&v| v.to_string()).collect::<Vec<String>>().join(", ");
+    let bbox = f.bbox.clone().unwrap_or(vec![]).iter().map(|&v| v.to_string()).collect::<Vec<String>>().join(",");
     // Feature properties:
     let ref properties = if let Some(properties) = &f.properties { properties } else { &Map::new() };
-    let platform_name: &str = properties["platformShortName"].as_str().unwrap();
-    let cloud_cover: f64 = properties["cloudCover"].as_f64().unwrap();
-    let capture_time: &str = properties["datetime"].as_str().unwrap();
-    return format!(
-        "type: {}, id: {}, cloudy: {}\ncapture time: {}, bbox: {}\n",
-        platform_name, id, cloud_cover, capture_time, bbox
-    );
+    let short_name: String = String::from(properties["platformShortName"].as_str().unwrap());
+    let serial_identifier: String = String::from(properties["platformSerialIdentifier"].as_str().unwrap());
+    let product_type: String = String::from(properties["productType"].as_str().unwrap());
+    let cloud_cover: String = properties["cloudCover"].as_f64().unwrap().to_string();
+    let capture_time: String = String::from(properties["datetime"].as_str().unwrap());
+    let quicklook_href: String = String::from(get_value(vec!["assets", "QUICKLOOK", "href"], &f.foreign_members).as_str().unwrap());
+    let product_href: String = String::from(get_value(vec!["assets", "PRODUCT", "href"], &f.foreign_members).as_str().unwrap());
+    let data = HashMap::from([
+        ("ID", id),
+        ("SHORT_NAME", short_name),
+        ("SERIAL", serial_identifier),
+        ("DETAIL", product_type),
+        ("CAPTURE_TIME", capture_time),
+        ("CLOUD_COVER", cloud_cover),
+        ("BBOX", bbox),
+        ("QUICKLOOK_HREF", quicklook_href),
+        ("PRODUCT_HREF", product_href)
+    ]);
+    return format_with_template(FEATURE_DETAILS_FORMAT, &data);
+}
+
+// Try this with pure accessor methods and the template at the top
+fn format_with_template(template: &str, data: &HashMap<&str, String>) -> String {
+    let mut compiled = String::from(template);
+    for (k, v) in data {
+        let tag = format!("<{}>", k);
+        compiled = compiled.replace(&tag, v);
+    }
+    return compiled;
 }
